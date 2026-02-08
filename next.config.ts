aab64a1958daf9a9d3afd1b9101a7faffc6017b1
Solodovsky <toxa2h@bk.ru>
2026-02-08 22:06:15 +0300

    fixed images

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    localPatterns: [
+      { pathname: "/img/**" },
+    ],
+  },
 };
 
 export default nextConfig;
