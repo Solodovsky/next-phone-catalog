f98efad1b40763c44adc6e87c4f97f32fd53fea0
Solodovsky <toxa2h@bk.ru>
2026-02-08 19:46:55 +0300

    fix navitem

--- src/app/components/header/Navitem.tsx
+++ src/app/components/header/Navitem.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "./Header.module.scss";
 
 type Props = {
@@ -9,14 +12,12 @@ type Props = {
 };
 
 const NavItem: React.FC<Props> = ({ to, children, end = false }) => {
+  const pathname = usePathname();
+  const isActive = end ? pathname === to : pathname.startsWith(to);
+  const className = `${styles.navLink} ${isActive ? styles.navLinkActive : ""}`;
+
   return (
-    <Link
-      href={to}
-      className={({ isActive }) =>
-        `${styles.navLink} ${isActive ? styles.navLinkActive : ""}`
-      }
-      end={end}
-    >
+    <Link href={to} className={className}>
       {children}
     </Link>
   );
